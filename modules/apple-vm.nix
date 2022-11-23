3fe17e15549ed84c1a398f0c49d080d8712a9908
sandydoo <hey@sandydoo.me>
2022-11-23 23:22:53 +0000

    Run x86 binaries via Rosetta

--- modules/apple-vm.nix
+++ modules/apple-vm.nix
@@ -1,13 +1,18 @@
-{ config, lib, pkgs, ... }:
+{ ... }:
 
 {
-  systemd.mounts = [
-    {
-      what = "ROSETTA";
-      where = "/mnt/rosetta";
-      type = "virtiofs";
-      wantedBy = [ "multi-user.target" ];
-      enable = true;
-    }
-  ];
+  fileSystems."/media/rosetta" =  {
+    device = "rosetta";
+    fsType = "virtiofs";
+  };
+
+  boot.binfmt.registrations.rosetta = {
+    interpreter = "/media/rosetta/rosetta";
+    magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
+    mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
+    preserveArgvZero = false;
+    matchCredentials = true;
+    fixBinary = true;
+    wrapInterpreterInShell = false;
+  };
 }
